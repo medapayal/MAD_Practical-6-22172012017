1e789a05a2a39d146918a5091e515994a0b41fd1
payal <payalbenmeda19@gnu.ac.in>
2023-09-21 23:26:51 +0530

    hey

---
+++ app/src/main/java/com/example/mad_practical_6_22172012017/MyService.kt
@@ -0,0 +1,12 @@
+package com.example.mad_practical_6_22172012017
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class MyService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
